c5fdcc443473cd8f9455454254ff2cc7d457ec37
mayankmohak <mayank8199@gmail.com>
2021-09-29 15:24:59 +0530

    bdjgd

---
+++ code.py
@@ -0,0 +1,14 @@
+#  Assignment 1
+'''
+arr = list(map(int,input("\nEnter the numbers : ").strip().split()))
+arr.sort()
+mid = len(arr) // 2
+res = (arr[mid] + arr[~mid]) / 2
+sum1 = int(sum(arr))
+len1 = int (len(arr))
+ans = sum1 // len1
+print("Mean of the list is : " + str(ans))
+print("Median of list is : " + str(res))
+'''
+#  Assignment 2
+
